97ada9d6885df50f639e99f597c768f3de2fbf25
tonytang9544 <tonytang9544@gmail.com>
2025-06-28 09:25:09 +0100

    nntcrdist works for smaller datasets

--- distance_phenotype/tcr_dist.py
+++ distance_phenotype/tcr_dist.py
@@ -1,10 +1,20 @@
 from pyrepseq.nn import nearest_neighbor_tcrdist
 import pandas as pd
+import numpy as np
+import os
 
-dataset_path = "/Users/tangm/The Francis Crick Dropbox/Minzhe Tang/Tony/After-PhD/Machine_learning_MSc/UCL-AI_for_biomed/Course_material/thesis_project/TCR_project/dataset/CD4_CD8_sceptr_nr_cdrs.csv.gz"
+dataset_root_path = "~/Documents/dataset"
+dataset_path = os.path.join(dataset_root_path, "CD4_CD8_sceptr_nr_cdrs.csv.gz")
+nn_array_path = os.path.join(dataset_root_path, "nn_array")
+# nn_array_path = os.path.realpath(nn_array_path)
+# print(nn_array_path)
 
-dataset = pd.read_csv(dataset_path)
+dataset = pd.read_csv(dataset_path).sample(1000000)
 
 print(dataset.head())
 
-nn_array = nearest_neighbor_tcrdist(dataset[:500], chain="both")
+nn_array = nearest_neighbor_tcrdist(dataset, chain="both", max_edits=2)
+
+print(nn_array)
+print(len(nn_array))
+np.save("nn_array", nn_array)
